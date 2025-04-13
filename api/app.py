9c5ee4e656eb1a3d1d692d44209b02ff57a4a07b
Ross Llewallyn <rossllewallyn@proton.me>
2025-04-12 21:52:00 -0400

    Flask app now loads agency info successfully

--- api/app.py
+++ api/app.py
@@ -4,11 +4,16 @@ Run the API to manage data CRUD requests
 Use via `python3 app.py` or `flask run`
 """
 
+import json
+import logging
 import os
+import requests
 import mysql.connector
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+
+logging.basicConfig(filename='flask_app.log', level=logging.DEBUG)
 
 MYSQL_DATABASE=os.environ.get('MYSQL_DATABASE', 'mydatabase')
 MYSQL_USER=os.environ.get('MYSQL_USER', 'myuser')
@@ -24,13 +29,27 @@ db_config = {
     'database': MYSQL_DATABASE
 }
 
+INIT_AGENCIES_URL="https://www.ecfr.gov/api/admin/v1/agencies.json"
+
 SUPPLY_INIT_DATA=True
 
 def get_db_connection():
     connection = mysql.connector.connect(**db_config)
     return connection
 
-init_agencies = []
+def _get_init_agency_data():
+    try:
+        response = requests.get(INIT_AGENCIES_URL)
+        response.raise_for_status()
+        data = response.json()
+
+        return data['agencies']
+    except Exception as e:
+        print(f"Error getting initial player data: {e}")
+        raise e
+
+def _clean_field(value):
+    return value if value is not None else ''
 
 def _initialize_db(supply_init_data = False):
     try:
@@ -46,7 +65,12 @@ def _initialize_db(supply_init_data = False):
         query_agencies = f"""CREATE TABLE IF NOT EXISTS {table_agencies} (
             id INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
-            type VARCHAR(255) NOT NULL
+            short_name VARCHAR(255) NOT NULL,
+            display_name VARCHAR(255) NOT NULL,
+            sortable_name VARCHAR(255) NOT NULL,
+            slug VARCHAR(255) NOT NULL,
+            cfr_references JSON NOT NULL,
+            parent_id INT
         )"""
         cursor.execute(query_agencies)
 
@@ -57,9 +81,66 @@ def _initialize_db(supply_init_data = False):
 
             # Only insert data during init if no data present
             if result[0] == 0:
-                query_add_agencies = f"INSERT INTO {table_agencies} (name, type) VALUES (%s, %s)"
-                agencies_data = [(agency['name'], agency['type']) for agency in init_agencies]
-                cursor.executemany(query_add_agencies, agencies_data)
+                # Get agency data
+                agencies = _get_init_agency_data()
+
+                if not agencies:
+                    print(f"Error getting initial agency data!")
+                    conn.commit()
+                    conn.close()
+                    return
+
+                query_add_agencies = f"INSERT INTO {table_agencies} \
+                    (name, short_name, display_name, sortable_name, slug, cfr_references) \
+                    VALUES ({'%s, %s, %s, %s, %s, %s'})"
+
+                # 1. Add top-level agencies
+                agency_data = [
+                    (
+                        agency['name'],
+                        _clean_field(agency.get('short_name', '')),
+                        agency['display_name'],
+                        agency['sortable_name'],
+                        agency['slug'],
+                        json.dumps(agency['cfr_references']),
+                    )
+                    for agency in agencies
+                ]
+                #print(agency_data)
+
+                cursor.executemany(query_add_agencies, agency_data)
+                conn.commit()
+
+                # Retrieve IDs for top-level agencies
+                cursor.execute(f"SELECT id, slug FROM {table_agencies}")
+                agency_id_map = {row[1]: row[0] for row in cursor.fetchall()}
+
+                # 2. Add child agencies with reference to parent
+                child_agencies = []
+                for agency in agencies:
+                    if 'children' in agency:
+                        for child in agency['children']:
+                            child['parent_id'] = agency_id_map.get(agency['slug'])
+                            child_agencies.append(child)
+                
+                child_agency_data = [
+                    (
+                        child['name'],
+                        _clean_field(child.get('short_name', '')),
+                        child['display_name'],
+                        child['sortable_name'],
+                        child['slug'],
+                        json.dumps(child['cfr_references']),
+                        child['parent_id'],
+                    )
+                    for child in child_agencies
+                ]
+                query_add_child_agencies = f"INSERT INTO {table_agencies} \
+                    (name, short_name, display_name, sortable_name, slug, cfr_references, parent_id) \
+                    VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                cursor.executemany(query_add_child_agencies, child_agency_data)
+                conn.commit()
+
                 print('Added initial agencies!')
 
         conn.commit()
