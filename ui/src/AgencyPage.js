e4ad8654f56d6a49e194f68df5868a14b517252b
Ross Llewallyn <rossllewallyn@proton.me>
2025-04-13 15:13:39 -0400

    year selector, word count total, formatting

--- ui/src/AgencyPage.js
+++ ui/src/AgencyPage.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 
 import './App.css';
+
+const startYear = 2015;
+const loadingText = "Loading...";
 
 function RegulationCard({ regulation }) {
   const referencePieces = [
@@ -19,15 +22,46 @@ function RegulationCard({ regulation }) {
     <div id={`regulation-${referenceSlug}`} className="Card">
       <div className="Container">
         <h2>{referenceText}</h2>
-        <p>{regulation.word_count}</p>
-        <h3>{regulation.date}</h3>
+        <h3>{formatWordCount(regulation.word_count)} words</h3>
+        <p>As of {formatDate(regulation.date)}</p>
       </div>
     </div>
   );
 }
 
+function YearSelector({ year, currentYear, setYear }) {
+  // Generate a list of years for the drop down
+  const dropdownYears = Array.from(
+    { length: currentYear - startYear + 1 },
+    (_, i) => startYear + i
+  );
+
+  return (
+    <div className="YearSelector">
+      <label htmlFor="year-select">Select Year: </label>
+      <select
+        id="year-select"
+        value={year}
+        onChange={(e) => setYear(e.target.value)}
+      >
+        {dropdownYears.map((y) => (
+          <option key={y} value={y}>{y}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+/// Accepts "?year=2022"
+/// TODOROSS: check child agencies
 function AgencyPage() {
   const { agencyId } = useParams();
+  const [searchParams] = useSearchParams();
+  const currentYear = new Date().getFullYear();
+  const initialYear = searchParams.get("year") || currentYear;
+  const [year, setYear] = useState(initialYear);
+
+  //const [loading, setLoading] = useState(true);
   const [agency, setAgency] = useState([]);
   const [regulations, setRegulations] = useState([]);
 
@@ -35,14 +69,25 @@ function AgencyPage() {
     fetch(`/agencies/${agencyId}`).then((res) => res.json()).then((data) => {
       setAgency(data);
     });
-    fetch(`/agencies/${agencyId}/regulations`).then((res) => res.json()).then((data) => {
+  }, [agencyId]);
+
+  useEffect(() => {
+    setRegulations([]); // Reset regulations when year changes
+    fetch(`/agencies/${agencyId}/regulations/${year}`).then((res) => res.json()).then((data) => {
       setRegulations(data);
     });
-  }, [agencyId]);
+  }, [agencyId, year]);
+
+  // Calculate total word count
+  const totalWordCount = regulations.reduce((total, regulation) => {
+    return total + (regulation.word_count || 0);
+  }, 0);
 
   return (
     <div id={`agency-regulations-${agency.slug}`}>
       <h1>{agency.short_name}</h1>
+      <h2>Total words: {totalWordCount === 0 ? loadingText : formatWordCount(totalWordCount)}</h2>
+      <YearSelector year={year} currentYear={currentYear} setYear={setYear}></YearSelector>
       <div className="Regulations">
         {regulations.map((item, index) => {
           return (
@@ -52,6 +97,32 @@ function AgencyPage() {
       </div>
     </div>
   );
+}
+
+function formatWordCount(count) {
+  if (count === null) {
+    return "N/A";
+  }
+  if (count > 1000000000) {
+    return `${(count / 1000000000).toFixed(1)}B`;
+  } else if (count > 1000000) {
+    return `${(count / 1000000).toFixed(1)}M`;
+  } else if (count > 1000) {
+    return `${(count / 1000).toFixed(1)}K`;
+  } else {
+    return count;
+  }
+}
+
+function formatDate(date) {
+  if (!date) return "N/A";
+
+  const d = new Date(date);
+  const year = d.getFullYear();
+  const month = d.toLocaleString("default", { month: "long" });
+  const day = d.getDate();
+
+  return `${month} ${day}, ${year}`;
 }
 
 export default AgencyPage;
